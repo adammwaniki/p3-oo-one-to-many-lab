255e14bc7632abc0994ed5af262a732356f53ba6
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-02 02:37:26 +0300

    Halfway done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,21 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+    
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    '''def pets(self):
+        return [pet for pet in self.pets if pet.owner == self.name]'''
+    
+    '''def add_pet(self,pet):
+        if pet in self.pets:
+            self.pets.append(pet)'''
